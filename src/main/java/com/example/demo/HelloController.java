d88a0985669cc3e79f75aa5b5302e6393c2bfd3d
Grzegorz Sadowski <gsadowski@op.pl>
2024-10-23 22:58:06 +0200

    Commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,10 +1,18 @@
 package com.example.demo;
 
+import com.example.demo.entities.User;
+import com.example.demo.service.UserService;
+import java.util.List;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class HelloController {
+
+    private final UserService userService;
+
 
     @GetMapping("/hello")
     public String sayHello() {
@@ -25,6 +33,11 @@ public class HelloController {
     public String getDevOpsValue() {
         String devOpsValue = System.getenv("DEVOPS");
         return "The value of DEVOPS is: " + (devOpsValue != null ? devOpsValue : "not set");
+    }
+
+    @GetMapping("/persons")
+    public List<User> getAllPersons() {
+        return userService.getAllUsers();
     }
 
 }
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import com.example.demo.entities.User;
+import com.example.demo.repository.UserRepository;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+
+
+    public List<User> getAllUsers() {
+            return userRepository.findAll();
+        }
+
+}
